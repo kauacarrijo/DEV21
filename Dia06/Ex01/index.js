416f23840b1a9e635cb2a7175e0b10db6ba2e286
kauacarrijo <kauazagocarrijo@gmail.com>
2024-05-31 23:48:25 -0300

    Dia06

---
+++ Dia06/Ex01/index.js
@@ -0,0 +1,6 @@
+let valorRecebido = Number(prompt("Digite um valor:"));
+let i;
+
+for(i = 0; i <= valorRecebido; i++) {
+    console.log(i);
+}
---
+++ Dia06/Ex04/index.js
@@ -0,0 +1,13 @@
+let i, numeroDeEntrada = Number(prompt("Digite um número"));
+console.log("Tabuada do número: " + numeroDeEntrada);
+for(i = 0; i <= 10; i++) {
+    console.log(`${numeroDeEntrada} x ${i} = ${numeroDeEntrada * i}`);
+}
+console.log("Tabuada do número: " + ++numeroDeEntrada);
+for(i = 0; i <= 10; i++) {
+    console.log(`${numeroDeEntrada} x ${i} = ${numeroDeEntrada * i}`);
+}
+console.log("Tabuada do número: " + ++numeroDeEntrada);
+for(i = 0; i <= 10; i++) {
+    console.log(`${numeroDeEntrada} x ${i} = ${numeroDeEntrada * i}`);
+}
