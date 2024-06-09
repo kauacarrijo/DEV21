ad57a72230fa6e31ddcc2257cf4d71e46827f59b
kauacarrijo <kauazagocarrijo@gmail.com>
2024-06-08 22:14:24 -0300

    Exercicio 4 Dia 10

--- Dia10/Ex04/index.js
+++ Dia10/Ex04/index.js
@@ -1,48 +1,11 @@
-let imc, anoDeNascimento, nome, idade, peso, altura, profissao, condicao;
-let anoAtual = 2024;
+let numeroAux, listaFibonacci, i;
+numeroAux = Number(prompt("Digite um número:"));
+listaFibonacci = [numeroAux - 1, numeroAux];
 
-do{
-    nome = prompt("Digite seu nome:");
-    idade = Number(prompt("Digite sua idade:"));
-    peso = Number(prompt("Digite seu peso: (Kg)"));
-    altura = Number(prompt("Digite sua altura: (cm)"));
-    profissao = prompt("Digite sua profissão:");
+for(i = 2; i < 10; i++){
+    listaFibonacci[i] = listaFibonacci[i - 2] + listaFibonacci[i - 1];
+}
 
-    console.log(`Olá ${nome}, você tem ${idade} anos, é ${profissao}, tem ${altura / 100}M de altura e pesa ${peso}kg.`);
-
-    if(idade >= 18){
-        console.log("Está liberado para tomar umas geladas.")
-    }else {
-        console.log("Sem gelada para você.")
-    }
-
-    console.log("Sua idade em meses: " + idade * 12);
-    console.log("Sua idade em semanas: " + idade * 52);
-    console.log("Sua idade em dias: " + idade * 365);
-
-    imc = peso / (2 * (altura / 100));
-
-    if(imc < 18.5){
-        console.log("Seu imc está menor que 18,5 Kg/m2, você está com Magreza");
-    }else {
-        if(imc <= 24.9){
-            console.log("Seu imc está entre 18,5 Kg/m2 e 24.9 Kg/m2, você está Normal");
-        }else{
-            if(imc <= 30){
-                console.log("Seu imc está entre 24.9 Kg/m2 e 30 Kg/m2, você está com Sobrepeso");
-            }else{
-                console.log("Seu imc está maior que 30 Kg/m2, você está com Obesidade");
-            }
-        }
-    }
-
-    anoDeNascimento = anoAtual - idade;
-
-    console.log("Seu ano de nascimento é: " + anoDeNascimento);
-
-    for(let i = anoDeNascimento, j = 0; i < anoAtual; i++, j++){
-        console.log(`Em ${i} você tinha ${j} anos de idade.`);
-    }
-
-    condicao = Number(prompt("Deseja continuar?\nDigite (0) para parar;\nDigite (1) para continuar."));
-}while(condicao);
+for(i = 0; i < 10; i++){
+    console.log(listaFibonacci[i]);
+}
